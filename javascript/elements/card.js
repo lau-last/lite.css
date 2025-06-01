1b821afafc930772c2794ec2c43e8702d265da8e
Laurent Lassallette <laurentlassallette@macbookpro.lan>
2025-06-01 22:17:21 +0200

    CSS <card-element></card-element>

--- javascript/elements/card.js
+++ javascript/elements/card.js
@@ -10,7 +10,7 @@ export default class CardElement extends HTMLElement {
         const footer = this.querySelector('card-footer')?.innerHTML || '';
 
         this.innerHTML = `
-            <div class="card">
+            <div class="card" style="width: 30rem;">
                 <div class="card-header">${header}</div>
                 <div class="card-body">${body}</div>
                 <div class="card-footer">${footer}</div>
